d275bf4b24c0448f36fc7a728c866c41604dac0a
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2015-11-22 13:04:21 +0100

    Make code PEP8 compliant

--- flappybird.py
+++ flappybird.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
+
 import pygame
-from pygame.locals import *
+from pygame.locals import *  # noqa
 import sys
 import random
+
 
 class FlappyBird:
     def __init__(self):
         self.screen = pygame.display.set_mode((400, 708))
         self.bird = pygame.Rect(65, 50, 50, 50)
         self.background = pygame.image.load("assets/background.png").convert()
-        self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(), pygame.image.load("assets/2.png").convert_alpha(), pygame.image.load("assets/dead.png")]
+        self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
+                            pygame.image.load("assets/2.png").convert_alpha(),
+                            pygame.image.load("assets/dead.png")]
         self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
         self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
         self.gap = 130
@@ -16,13 +21,13 @@ class FlappyBird:
         self.birdY = 350
         self.jump = 0
         self.jumpSpeed = 10
-        self.gravity= 5
+        self.gravity = 5
         self.dead = False
         self.sprite = 0
         self.counter = 0
         self.offset = random.randint(-110, 110)
 
-    def updateWalls(self):  
+    def updateWalls(self):
         self.wallx -= 2
         if self.wallx < -80:
             self.wallx = 400
@@ -38,8 +43,14 @@ class FlappyBird:
             self.birdY += self.gravity
             self.gravity += 0.2
         self.bird[1] = self.birdY
-        upRect = pygame.Rect(self.wallx, 360 + self.gap - self.offset + 10, self.wallUp.get_width() - 10, self.wallUp.get_height())
-        downRect = pygame.Rect(self.wallx, 0 - self.gap - self.offset - 10, self.wallDown.get_width() - 10, self.wallDown.get_height())
+        upRect = pygame.Rect(self.wallx,
+                             360 + self.gap - self.offset + 10,
+                             self.wallUp.get_width() - 10,
+                             self.wallUp.get_height())
+        downRect = pygame.Rect(self.wallx,
+                               0 - self.gap - self.offset - 10,
+                               self.wallDown.get_width() - 10,
+                               self.wallDown.get_height())
         if upRect.colliderect(self.bird):
             self.dead = True
         if downRect.colliderect(self.bird):
@@ -67,11 +78,16 @@ class FlappyBird:
                     self.gravity = 5
                     self.jumpSpeed = 10
 
-            self.screen.fill((255,255,255))
+            self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
-            self.screen.blit(self.wallUp, (self.wallx, 360 + self.gap - self.offset)) 
-            self.screen.blit(self.wallDown, (self.wallx, 0 - self.gap - self.offset))
-            self.screen.blit(font.render(str(self.counter), -1, (255,255,255)), (200, 50))           
+            self.screen.blit(self.wallUp,
+                             (self.wallx, 360 + self.gap - self.offset))
+            self.screen.blit(self.wallDown,
+                             (self.wallx, 0 - self.gap - self.offset))
+            self.screen.blit(font.render(str(self.counter),
+                                         -1,
+                                         (255, 255, 255)),
+                             (200, 50))
             if self.dead:
                 self.sprite = 2
             elif self.jump:
